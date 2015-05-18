f0efe190f416057c9679ec600368cecd74cfede1
NoTrees <this.is.ece@gmail.com>
2015-05-18 17:17:52 +0800

    Fixing test helper!

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,12 +5,25 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  setup { host! 'api.example.com' }
-  setup { @ip = '123.123.12.12' }
-  setup { @employee = Employee.create( id: '84438', name: 'James', division: 'SSF', authentication: '1344455' ) }
-  setup { @work_time = WorkTime.create( employee_id: '12345', time_of_scan: '08:00:00', time_flag: 'logged_out', work_date: '2015-05-18' ) }
-  setup { @user = User.create! }
-  setup { @auth_header = "Token token=#{@user.auth_token}" }
+  
+  setup do 
+    host! 'api.example.com' 
+    @ip = '123.123.12.12'
+    @employee = Employee.create( 
+                                  id: '84438', 
+                                  name: 'James', 
+                                  division: 'SSF', 
+                                  authentication: '1344455' 
+                               )
+    @work_time = WorkTime.create( 
+                                  employee_id: '12345', 
+                                  time_of_scan: '08:00:00', 
+                                  time_flag: 'logged_out', 
+                                  work_date: '2015-05-18' 
+                                )
+    @user = User.create!
+    @auth_header = "Token token=#{@user.auth_token}"
+  end
 
   # Add more helper methods to be used by all tests here...
   def json(body)
