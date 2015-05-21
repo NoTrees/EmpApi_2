9b5b16be531e7ce992e670751979b93b73e4bf6a
NoTrees <this.is.ece@gmail.com>
2015-05-21 10:08:29 +0800

    Update gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 require 'api_version'
 
 Rails.application.routes.draw do
-  namespace :api, path: '/', constraints: { subdomain: 'api' } do
+  root 'application#hello'
+
+  namespace :api, path: '/' do#, constraints: { subdomain: 'api' } do
     scope module: :v1, constraints: ApiVersion.new('v1', true) do
       resources :work_times
       resources :employees
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create()
+
+Employee.create(
+	id: '12345',
+  	name: 'Emily',
+  	division: 'ERD',
+  	authentication: '1234567890',
+  	address: '#123 Drive Street'
+  	)
+
+WorkTime.create(
+    employee_id: "12345",
+    time_of_scan: '08:00:00',
+    time_flag: 'logged_in',
+  	work_date: 2015-05-14
+  	)
+
+WorkTime.create(
+    employee_id: "12345",
+    time_of_scan: '17:00:00',
+    time_flag: 'logged_out',
+  	work_date: 2015-05-14
+  	)
